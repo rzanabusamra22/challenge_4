f36103ef2f83dcd6898383d650664443995fe66f
rzanabusamra22 <rzanabusamra@gmail.com>
2020-11-05 15:32:31 +0200

    praying to submmit something

--- public/game.js
+++ public/game.js
@@ -1,2 +1,34 @@
-var Player1 = prompt('Player One: Enter your Name')
-var Player2 = prompt('Player One: Enter your Name')
+var player1 = prompt("Player One : Enter Your Name");
+console.log("Player One is :- " + player1)
+var player1Color = 'rgb(86, 151, 255)';
+var player2 = prompt("Player Two : Enter Your Name");
+console.log("Player Two is :- " + player2)
+var player2Color = 'rgb(237, 45, 73)';
+var game_on = true;
+var table = $('table tr');
+
+function reportwin(rowNum, colNum) {
+    console.log(currentName + " You won starting at this row,col :- (" + (rowNum + 1) + "," + (colNum + 1) + ")");
+}
+
+function changeColor(rowIndex, colIndex, color) {
+    return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color', color);
+}
+
+function returnColor(rowIndex, colIndex, color) {
+    return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
+}
+function checkBottom(colIndex) {
+    var colorReport = returnColor(5, colIndex)
+    for (var row = 5; row > -1; row--) {
+        colorReport = returnColor(row, colIndex);
+        if (colorReport === 'rgb(128, 128, 128)') {
+            return row
+        }
+    }
+}
+///
+function colorMatchCheck(one, two, three, four) {
+    return (one === two && one === three && one === four && one !== 'rgb(128, 128, 128)' && one !== undefined)
+}
+
