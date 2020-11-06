cd033e4b953f09ee0a97f59374629c7a20c1a956
rzanabusamra22 <rzanabusamra@gmail.com>
2020-11-06 17:33:38 +0200

    not working sorry

--- public/game.js
+++ public/game.js
@@ -30,7 +30,79 @@ function checkBottom(colIndex) {
 function colorMatchCheck(one, two, three, four) {
     return (one === two && one === three && one === four && one !== 'rgb(128, 128, 128)' && one !== undefined)
 }
+function horizontalWinCheck() {
+    for (var row = 0; row < 6; row++) {
+        for (var col = 0; col < 4; col++) {
+            if (colorMatchCheck(returnColor(row, col), returnColor(row, col + 1), returnColor(row, col + 2), returnColor(row, col + 3))) {
+                console.log('Horizontal Win');
+                reportwin(row, col);
+                return true;
+            } else {
+                continue;
+            }
+        }
+    }
+}
+function verticalWinCheck() {
+    for (var col = 0; col < 7; col++) {
+        for (var row = 0; row < 3; row++) {
+            if (colorMatchCheck(returnColor(row, col), returnColor(row + 1, col), returnColor(row + 2, col), returnColor(row + 3, col))) {
+                console.log('Vertical Win');
+                reportwin(row, col);
+                return true;
+            } else {
+                continue;
+            }
+        }
+    }
+}
+function diagonalWinCheck() {
+    for (var col = 0; col < 5; col++) {
+        for (var row = 0; row < 7; row++) {
+            if (colorMatchCheck(returnColor(row, col), returnColor(row + 1, col + 1), returnColor(row + 2, col + 2), returnColor(row + 3, col + 3))) {
+                console.log('Diagonal Win');
+                reportwin(row, col);
+                return true;
+            } else if (colorMatchCheck(returnColor(row, col), returnColor(row - 1, col + 1), returnColor(row - 2, col + 2), returnColor(row - 3, col + 3))) {
+                console.log('Diagonal Win');
+                reportwin(row, col);
+                return true;
+            } else {
+                continue;
+            }
+        }
+    }
+}
+var currentPlayer = 1;
+var currentName = player1;
+var currentColor = player1Color;
 
+$('h3').text(player1 + " it is your turn, pick a column to drop in!")
+
+$('.board button').on('click', function () {
+    var col = $(this).closest('td').index();
+    var bottomAvail = checkBottom(col);
+    changeColor(bottomAvail, col, currentColor);
+    if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()) {
+        $('h1').text(currentName + " You have Won!")
+        //console.log(currentName+" You have Won!")
+        $('h3').fadeOut(20);
+        game_on = false;
+    }
+    currentPlayer = currentPlayer * -1;
+    if (currentPlayer === 1) {
+        currentName = player1;
+        $('h3').text(currentName + " it is your turn, pick a column to drop in!")
+        currentColor = player1Color;
+    } else {
+        currentName = player2;
+        $('h3').text(currentName + " it is your turn, pick a column to drop in!")
+        currentColor = player2Color;
+    }
+    if (game_on === false) {
+        $('h2').text("Refresh the page to Play Again!")
+    }
+})
 function replay() {
     location.reload()
 }
